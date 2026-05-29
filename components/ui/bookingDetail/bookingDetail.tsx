8f9efe6e521c95d7c0baf56ef8c841ac4ae0cee1
saye <saje.saadat@lexlink.se>
2026-05-29 16:20:54 +0200

    add bookingDetail component

---
+++ components/ui/bookingDetail/bookingDetail.tsx
@@ -0,0 +1,305 @@
+import {
+  Calendar,
+  CircleCheck,
+  CircleX,
+  Clock,
+  Clock3,
+  Icon,
+  MoveLeft,
+  ShieldCheck,
+  Users,
+} from "lucide-react";
+import { Card, CardContent, CardHeader } from "../card/card";
+import { DropdownMenu } from "radix-ui";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "../accordion";
+import { Button } from "../button/button";
+import { Badge } from "../badge/badge";
+
+export type BookingStates =
+  | "waiting_for_payment"
+  | "pending_payment"
+  | "confirmed"
+  | "cancel_pending_refund"
+  | "cancelled"
+  | "cancellation_failed";
+
+export type BookingDetailProps = {
+  requestId: string; //BookingId
+  title: string;
+  status: BookingStates;
+  eventDate: string;
+  number_of_guests: number;
+  totalAmount: string;
+  basePrice: string;
+  pricePerGuest: string;
+  weekdayModifier: string;
+  platformFee: string;
+  chefFee: string;
+  onClick?: () => void;
+  onCancel?: () => void;
+};
+
+//  map each booking status to its display label, badge style, and icon
+const statusStyle: Record<
+  BookingDetailProps["status"],
+  { label: string; className: string; icon: React.ReactNode }
+> = {
+  confirmed: {
+    label: "Bekräftad",
+    className:
+      "h-auto w-fit px-3 py-1 relative bg-emerald-100 text-green-800 rounded-xl inline-flex  justify-start items-start leading-4 ",
+    icon: <CircleCheck className=" size-5 text-emerald-500" />,
+  },
+  cancelled: {
+    label: "Inställd",
+    className:
+      "h-auto w-fit px-3 py-1 relative bg-red-100 text-red-800 rounded-xl inline-flex  justify-start items-start leading-4 ",
+    icon: <CircleX className="size-5  text-red-500" />,
+  },
+  pending_payment: {
+    label: "Väntar på betalning",
+    className:
+      "h-auto w-fit px-3 py-1 relative bg-yellow-100 text-yellow-800 rounded-xl inline-flex  justify-start items-start leading-4 ",
+    icon: <Clock3 className="size-5  text-yellow-500" />,
+  },
+  waiting_for_payment: {
+    label: "Väntar på betalning",
+    className:
+      "h-auto w-fit px-3 py-1 relative bg-yellow-100 text-yellow-800 rounded-xl inline-flex  justify-start items-start leading-4 ",
+    icon: <Clock3 className="size-5  text-yellow-500" />,
+  },
+  cancel_pending_refund: {
+    label: "Avbokning pågår",
+    className:
+      "h-auto w-fit px-3 py-1 relative bg-orange-100 text-orange-800 rounded-xl inline-flex  justify-start items-start leading-4 ",
+    icon: <CircleX className="size-5  text-red-500" />,
+  },
+  cancellation_failed: {
+    label: "Avbokning misslyckades",
+    className:
+      "h-auto w-fit px-3 py-1 relative bg-red-100 text-red-800 rounded-xl inline-flex  justify-start items-start leading-4 ",
+    icon: <CircleX className="size-5  text-red-500" />,
+  },
+};
+
+export function BookingDetail({
+  requestId,
+  title,
+  status,
+  eventDate,
+  number_of_guests,
+  totalAmount,
+  basePrice,
+  pricePerGuest,
+  weekdayModifier,
+  platformFee,
+  chefFee,
+  onClick,
+  onCancel,
+}: BookingDetailProps) {
+  // Accordion item in a Card component.
+  const item = [
+    {
+      value: "plans",
+      trigger: (
+        <div className="flex flex-1 justify-between pr-2 font-semibold font-['Inter'] leading-6">
+          <span>Total</span>
+          <span>{totalAmount}</span>
+        </div>
+      ),
+      content: (
+        <div className="flex flex-col gap-2 text-sm pt-2">
+          {/* Base Price */}
+          <div className="self-stretch inline-flex justify-between items-start">
+            <div className="justify-center text-zinc-600 text-base font-normal font-['Inter'] leading-6">
+              Baspris
+            </div>
+            <div className="justify-center text-stone-900 text-base font-medium font-['Inter'] leading-6">
+              {basePrice}
+            </div>
+          </div>
+
+          {/* Price per guest*/}
+          <div className="self-stretch inline-flex justify-between items-start">
+            <div className="justify-center text-zinc-600 text-base font-normal font-['Inter'] leading-6">
+              Pris per gäst
+            </div>
+            <div className="justify-center text-stone-900 text-base font-medium font-['Inter'] leading-6">
+              {pricePerGuest}
+            </div>
+          </div>
+
+          {/* weekday Modifier*/}
+          <div className="self-stretch inline-flex justify-between items-start">
+            <div className="justify-center text-zinc-600 text-base font-normal font-['Inter'] leading-6">
+              Veckodagsmodifierare
+              <br />
+              (Lördag)
+            </div>
+            <div className="self-stretch pl-2.5 inline-flex flex-col justify-start items-end">
+              <div className="text-right justify-center text-stone-900 text-base font-medium font-['Inter'] leading-6">
+                {weekdayModifier}{" "}
+              </div>
+              <div className="text-right justify-center text-zinc-600 text-[7px] font-normal font-['Inter'] uppercase leading-4">
+                (APPLICERAS PÅ PER-GÄST DELEN AV PRISET)
+              </div>
+            </div>
+          </div>
+
+          {/* Total Amount*/}
+          <div className="self-stretch pt-4 border-t border-stone-200 inline-flex justify-between items-center">
+            <div className="justify-center text-stone-900 text-base font-semibold font-['Inter'] leading-6">
+              Total
+            </div>
+            <div className="justify-center text-stone-900 text-base font-semibold font-['Inter'] leading-6">
+              {totalAmount}
+            </div>
+          </div>
+
+          {/* platform Fee */}
+          <div className="self-stretch inline-flex justify-between items-start">
+            <div className="justify-center text-zinc-600 text-base font-normal font-['Inter'] leading-6">
+              Plattformsavgift
+            </div>
+            <div className="justify-center text-zinc-600 text-base font-medium font-['Inter'] leading-6">
+              {platformFee}
+            </div>
+          </div>
+
+          {/* chef Fee */}
+          <div className="self-stretch pt-6 border-t border-stone-200 inline-flex justify-between items-center">
+            <div className="justify-center text-stone-900 text-base font-normal font-['Inter'] leading-6">
+              Kockens arvode
+            </div>
+            <div className="justify-center text-yellow-800 text-base font-normal font-['Inter'] leading-6">
+              {chefFee}
+            </div>
+          </div>
+        </div>
+      ),
+    },
+  ];
+  return (
+    <div className="w-96 h-314.25 relative bg-stone-50 inline-flex flex-col justify-start items-start">
+      {/* Header */}
+      <div className="w-96 h-16 left-0 top-0 absolute bg-stone-50 border-b border-stone-200">
+        <Button variant={"ghost"} onClick={onClick}>
+          <MoveLeft className="text-yellow-500 text-3xl size-4 left-6 top-[23.50px] absolute " />
+        </Button>
+        <div className="left-16 top-[17.50px] absolute justify-center text-stone-900 text-xl font-semibold font-['Inter'] leading-7 tracking-wide">
+          Booking Detail
+        </div>
+      </div>
+      {/* Status + Booking ID */}
+      <div className="self-stretch mt-16 h-20 inline-flex justify-between items-center">
+        {/* <div className="w-44 inline-flex flex-col justify-start items-start">
+          <div className="self-stretch justify-center text-zinc-600 text-xs font-normal font-['Inter'] uppercase leading-4 tracking-wide">
+            BOOKING STATUS
+          </div>
+          <div className="size- pt-1 flex flex-col justify-start items-start">
+            <div className="size- shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] inline-flex justify-start items-center">
+              <CircleCheck className="size-5" />
+              <div className="w-24 h-12 px-3 py-1 relative bg-emerald-100 rounded-xl inline-flex flex-col justify-start items-start">
+                <div className="left-4.5 top-4 absolute justify-center text-emerald-800 text-base font-semibold font-['Inter'] leading-4">
+                  {status}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div> */}
+        <div className=" inline-flex justify-start items-center">
+          {/* <CircleCheck className="size-5" /> */}
+          {/* <div className="w-24 h-12 px-3 py-1 relative bg-emerald-100 rounded-xl inline-flex flex-col justify-start items-start"> */}
+          {/* <Badge className="left-4.5 top-4 absolute justify-center text-emerald-800 text-base font-semibold font-['Inter'] leading-4"> */}
+          {statusStyle[status].icon}
+          <Badge className={statusStyle[status].className}>
+            {statusStyle[status].label}
+          </Badge>
+
+          {/* </div> */}
+        </div>
+        <div className="w-28 inline-flex flex-col justify-start items-end gap-[0.50px]">
+          <div className="self-stretch text-right justify-center text-zinc-600 text-xs font-normal font-['Inter'] uppercase leading-4 tracking-wide">
+            BOOKING ID
+          </div>
+          <div className="text-right justify-center text-stone-900 text-base font-medium font-['Inter'] leading-6">
+            {requestId.slice(0, 6)}...
+          </div>
+        </div>
+      </div>
+
+      {/* Main Card */}
+      <Card className="self-stretch px-6 pt-8 pb-6 bg-white rounded-lg shadow-[0px_4px_20px_0px_rgba(0,0,0,0.04)] outline -outline-offset-1 outline-gray-200 inline-flex flex-col justify-start items-start gap-6">
+        {/* cardHeader */}
+        <CardHeader className="self-stretch border-b border-gray-200 inline-flex flex-col justify-start items-start gap-1">
+          <div className="self-stretch justify-center text-stone-900 text-3xl font-semibold font-['Inter'] leading-8">
+            {title}
+          </div>
+          <div className="self-stretch inline-flex justify-start items-center gap-1.5">
+            <div className="size- inline-flex flex-col justify-center items-center">
+              <Calendar className="w-3.5 h-4" />
+            </div>
+            <div className="size- inline-flex flex-col justify-start items-start gap-0.5">
+              <div className="justify-center text-stone-900 text-base font-normal font-['Inter'] leading-6">
+                {eventDate}
+              </div>
+            </div>
+          </div>
+          <div className="self-stretch inline-flex justify-start items-center gap-1.5">
+            <div className="w-4 pr-3 pt-0.5 inline-flex flex-col justify-start items-start">
+              <Users className="w-5 h-3.5" />
+            </div>
+            <div className="size- inline-flex flex-col justify-start items-start gap-0.5">
+              <div className="justify-center text-stone-900 text-base font-normal font-['Inter'] leading-6">
+                {number_of_guests} Personer
+              </div>
+            </div>
+          </div>
+        </CardHeader>
+        {/* cardCotent */}
+        <CardContent className="self-stretch p-6 bg-white rounded-lg shadow-[0px_4px_20px_0px_rgba(211,197,172,0.31)] outline -outline-offset-1 outline-stone-200 inline-flex flex-col justify-start items-start gap-1">
+          <div className="self-stretch justify-center text-yellow-800 text-xs font-normal font-['Inter'] uppercase leading-4 tracking-[2.40px]">
+            PRISDETALJER
+          </div>
+          <div className="self-stretch justify-center text-zinc-600 text-sm font-light font-['Inter'] leading-5">
+            Detaljerad prisuppdelning för denna bokning
+          </div>
+          <div className="self-stretch flex flex-col justify-start items-start gap-4">
+            <div className="self-stretch  px-3.5 py-2.5 bg-white rounded-sm inline-flex justify-center items-center">
+              <div className="flex-1 justify-center text-stone-900 text-base font-semibold font-['Inter'] leading-6">
+                {/* {totalAmount} */}
+                <Accordion type="single" collapsible defaultValue="plans">
+                  {item.map((item) => (
+                    <AccordionItem key={item.value} value={item.value}>
+                      <AccordionTrigger>{item.trigger}</AccordionTrigger>
+                      <AccordionContent>{item.content}</AccordionContent>
+                    </AccordionItem>
+                  ))}
+                </Accordion>
+              </div>
+
+              <div className="size- px-2 py-2.5 flex justify-start items-center gap-2.5 overflow-hidden">
+                <div className="size-6 relative overflow-hidden">
+                  {/* <div className="w-2.5 h-[4.80px] left-[7.20px] top-[9.60px] absolute outline outline-offset-[-0.90px] outline-gray-500" /> */}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="self-stretch h-11 p-3 bg-stone-200/10 inline-flex justify-center items-center">
+            <div className="size- pr-2 inline-flex flex-col justify-start items-start">
+              <ShieldCheck className="w-3.5 h-4" />
+            </div>
+            <div className="justify-center text-gray-600 text-sm font-normal font-['Inter'] leading-5">
+              Secured by Stripe Payments
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
